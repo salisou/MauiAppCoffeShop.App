710ed9a99ee42e2b930f1aa463db499c2f7c3ec9
Moussa <salisoumoussa1@gmail.com>
2023-07-25 17:49:58 +0200

    Aggiungere i file di progetto.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiAppCoffeShop;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace MauiAppCoffeShop;
+
+public partial class MainPage : ContentPage
+{
+	
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	
+}
+
